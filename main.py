0aeec8b8647ca3bd558c6fab629ce50ab34aec6f
Maxim <pankrukhin.maxim@gmail.com>
2022-07-05 20:36:46 +0300

    Сырой продукт

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import vk_api
+import os.path
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 from vk_api.utils import get_random_id
 
@@ -12,25 +13,17 @@ class Main:
         self.longpoll = VkBotLongPoll(self.vk_session, '214336365')
         self.vk_api = self.vk_session.get_api()
 
-    def send_msg(self, send_id, msg, rand):
-        if rand == 1:
-            self.vk_api.messages.send(peer_id=send_id, message=msg, random_id=rand)
-        else:
-            self.vk_api.messages.send(peer_id=send_id, message=msg, random_id=get_random_id())
+    def send_msg(self, send_id, msg):
+        self.vk_api.messages.send(peer_id=send_id, message=msg, random_id=get_random_id())
 
     def events(self):
         while True:
             for event in self.longpoll.listen():
-                if event.type == VkBotEventType.MESSAGE_NEW and event.object['message'] and event.from_chat or event.type == VkBotEventType.MESSAGE_NEW and event.object['message']:
+                if event.type == VkBotEventType.MESSAGE_NEW and event.object['message']:
                     message = event.obj['message']
 
-                    peer = event.object.message['peer_id']
                     id = event.object.message['from_id']
                     text = message['text']
-
-                    main.send_msg(peer,
-                    "Приветствую&#128400;, я чат-бот, который поможет тебе оформить документ в соответствии с ГОСТом."
-                    "\nОтправь сообщение \"help\", чтобы я смог рассказать правила пользования чат-ботом.", 1)
 
                     if text.lower() == 'help':
                         main.send_msg(id,
@@ -41,16 +34,33 @@ class Main:
                                       "\npatterns — посмотреть список сохраненных шаблонов и шаблонов по умолчанию;"
                                       "\ncreate — создать и сохранить новый шаблон;"
                                       "\nsupport — вызов поддержки для решения проблем с работой чат-бота."
-                                      "\n\nНам важна ваша конфиденциальность и эффективность работы бота. Бот не перенаправляет ваши файлы и данные другим пользователям и группам, а сообщения с готовыми файлами автоматически удаляются спустя сутки после оформления работы.", 0)
-                    elif text.lower() == 'settings':
-                        main.send_msg(id, "Чат-бот предоставляет возможность настроить свою работу для вашего удобства."
-                                          "\n\nНастройки:"
-                                          "\nbuttons — включить/отключить систему кнопок;"
-                                          "\nadvise — включить/отключить подсказки.", 0)
+                                      "\n\nНам важна ваша конфиденциальность и эффективность работы бота. "
+                                      "Бот не перенаправляет ваши файлы и данные другим пользователям и группам,"
+                                      "а сообщения с готовыми файлами автоматически удаляются спустя сутки после оформления работы, при этом все шаблоны сохраняются.")
+                    elif text.lower() == 'patterns':
+                        fpath = "/home/Klopolupka/patterns/" + str(id)
+                        if os.path.exists(fpath):
+                            pattern = open(fpath, "r+")
+                            main.send_msg(id, pattern.readline())
+                            pattern.close()
+                        else:
+                            main.send_msg(id, "У Вас 0 сохраненных шаблонов. Введите 'add pattern', чтобы добавить новый шаблон.")
+                    elif text.lower() == 'add pattern':
+                            pattern = open(fpath, "w+")
+                            pattern.close()
                     elif text.lower() == 'support':
-                        vk = self.vk_session.get_api()
-                        main.send_msg(id, "Ваш запрос на техническую поддержку обрабатывается. В ближайшее время с вами свяжется технический специалист. Ожидайте...", 0)
-                        main.send_msg(208567150, "Поступил запрос на техническую поддержку от пользователя под id – " + str(id), 0)
+                        main.send_msg(id, "Ваш запрос на техническую поддержку обрабатывается. В ближайшее время с вами свяжется технический специалист. Ожидайте...")
+                        main.send_msg(208567150, "Поступил запрос на техническую поддержку от пользователя под id – " + str(id))
+                    elif text.lower() == 'commands':
+                        main.send_msg(id,
+                        "\n\nНа данный момент чат-бот поддерживает следующий список команд:"
+                        "\nprocess — начать процесс обработки документов;"
+                        "\npatterns — посмотреть список сохраненных шаблонов и шаблонов по умолчанию;"
+                        "\ncreate — создать и сохранить новый шаблон;"
+                        "\nsupport — вызов поддержки для решения проблем с работой чат-бота.")
+                    else:
+                        main.send_msg(id, "Данная команда не найдена. Посмотреть список команд можно введя 'help' или 'commands'."
+                        "\nЕсли Вы уверены, что ввели команду верно, вызовите техническую поддержку – 'support'.")
 
 
 if __name__ == "__main__":
